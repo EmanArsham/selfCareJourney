42c3e79ae212612ffdb5288b216768ce01b00efa
EmanArsham <earsh2@uic.edu>
2024-07-22 11:06:06 -0500

    Initial Commit

---
+++ button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by eman arsham on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
